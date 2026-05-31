60a841d4749f660715522d2c7eb3129cb402b4d6
261166 <289014476+261166@users.noreply.github.com>
2026-05-31 10:53:06 +0200

    Merge pull request #2 from rafal-nowak/sub
    added subtraction

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ void usage(char *programName) {
     printf("Usage: %s number1 operation number2\n", programName);
     printf("Available operations:\n");
     printf("\tadd\n");
+    printf("\tsub\n");
     printf("\n");
 }
 
@@ -21,6 +22,12 @@ int main(int argc, char* argv[]) {
         if (!strcmp("add", argv[2])) {
             result = number1 + number2;
             printf("%d + %d = %d\n", number1, number2, result);
+            return 0;
+        }
+
+        if (!strcmp("sub", argv[2])) {
+            result = number1 - number2;
+            printf("%d - %d = %d\n", number1, number2, result);
             return 0;
         }
 
